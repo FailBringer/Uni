3ed0b47c7d59c81886c2d622986b278e8da4c8c8
FailBringer <fordocs1234@gmail.com>
2016-06-07 19:13:49 +0300

    From Home To Work

--- Assets/CardView.cs
+++ Assets/CardView.cs
@@ -6,6 +6,7 @@ public class CardView : MonoBehaviour {
 	// Use this for initialization
 	public Sprite skin;
 	public Sprite cardSprite;
+	private bool rotate;
 	void Start () {
 	
 	}
@@ -21,10 +22,19 @@ public class CardView : MonoBehaviour {
 		RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 		if (hit.collider != null) {
 			Debug.Log ("HitFound");
-			if(hit.transform.parent.name == "MyCardPlaces"){
-				Animate();
-				Debug.Log ("Ray Hit " + hit.transform.name);
-				cardSprite = hit.transform.GetComponent<Place>().GetCard ().GetComponent<SpriteRenderer>().sprite;
+			if (hit.transform.name == "CardInfo" && !rotate) {
+				Rotate ();
+				rotate = true;
+			} else if (hit.transform.name == "CardInfo" && rotate) {
+				RotateBack();
+				rotate = false;
+			} 
+			else {
+				if (hit.transform.parent.name == "MyCardPlaces") {
+					Animate ();
+					Debug.Log ("Ray Hit " + hit.transform.name);
+					cardSprite = hit.transform.GetComponent<Place> ().GetCard ().GetComponent<SpriteRenderer> ().sprite;
+				}
 			}
 		}
 	}
@@ -32,6 +42,7 @@ public class CardView : MonoBehaviour {
 	public void StopAnimation() {
 		Animator animator = GetComponent<Animator> ();
 		animator.SetBool ("Change", false);
+		//transform.rotation = Quaternion.Euler (0, 0, 0);
 	}
 
 	public void Animate() {
@@ -45,4 +56,28 @@ public class CardView : MonoBehaviour {
 	public void ShowCard(){
 		GetComponent<SpriteRenderer> ().sprite = cardSprite;
 	}
+
+	public void Rotate(){
+		Animator animator = GetComponent<Animator> ();
+			animator.SetBool("Rotate", true);
+	}
+
+	public void RotateBack(){
+		Animator animator = GetComponent<Animator> ();
+			animator.SetBool("RotateBack", true);
+	}
+
+	public void StopRotate(){
+		Animator animator = GetComponent<Animator> ();
+		if (animator.GetBool ("Rotate")) {
+			animator.SetBool ("Rotate", false);
+			transform.rotation = Quaternion.Euler (0, 0, 180);
+		}
+		if (animator.GetBool ("RotateBack")) {
+			animator.SetBool("RotateBack", false);
+			transform.rotation = Quaternion.Euler (0,0,0);
+		}
+	}
+	
+
 }
